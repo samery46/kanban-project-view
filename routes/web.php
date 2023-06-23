e1527e3cd4eb031ecd837f7486914db5434a3c68
samery46 <syamme@ymail.com>
2023-06-23 16:19:17 +0700

    Apply common layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController; // Ditambahkan
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home'); // Diperbaharui
-});
+})->name('home'); // name ditambahkan
+
+Route::get('/tasks/', [TaskController::class, 'index'])->name('tasks.index'); // name Ditambahkan
